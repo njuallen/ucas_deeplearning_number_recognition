67a7b94613dff0fc8696030d2c447784da4ea164
Zhigang Liu <njuallen@foxmail.com>
2019-05-28 02:19:03 +0000

    Added annotations and fixed indentations.

--- numberRec.py
+++ numberRec.py
@@ -16,30 +16,49 @@ def mean_normalize(features):
     mean_value = features.mean()
     return (features - mean_value) / std_value
 
+class CNNConfig():
+    # 网络结构
+    filter_sizes = [2, 3, 4, 5]
+    num_filters = 64
+    hidden_dim = 256
+
+    # 训练过程
+    learning_rate = 0.001
+    num_epochs =
+    dropout_keep_prob = 0.5
+    print_per_batch = 100       # 训练过程中,每100次batch迭代，打印训练信息
+    save_tb_per_batch = 200
+
 class ASRCNN(object):
     def __init__(self, config, width, height, num_classes):  # 20,100
         self.config = config
+        # 训练过程
+        # 输入的语音变成了一张特征图
         self.input_x = tf.placeholder(tf.float32, [None, width, height], name='input_x')
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name='input_y')
         self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
         # input_x = tf.reshape(self.input_x, [-1, height, width])
+        # 将图由width * height变成了height * width
         input_x = tf.transpose(self.input_x, [0, 2, 1])
         pooled_outputs = []
+        # 也就是说有一系列不同size的filter
         for i, filter_size in enumerate(self.config.filter_sizes):
             with tf.name_scope("conv-maxpool-%s" % filter_size):
                 print("conv-maxpool-%s" % filter_size)
                 conv = tf.layers.conv1d(input_x, self.config.num_filters, filter_size, activation=tf.nn.relu)
+                print(conv.shape)
                 pooled = tf.reduce_max(conv, reduction_indices=[1])
+                print(pooled.shape)
                 pooled_outputs.append(pooled)
         num_filters_total = self.config.num_filters * len(self.config.filter_sizes)  # 64*4
+        print(pooled_outputs.shape)
         pooled_reshape = tf.reshape(tf.concat(pooled_outputs, 1), [-1, num_filters_total])
-        #pooled_flat = tf.nn.dropout(pooled_reshape, self.keep_prob)
+        print(pooled_reshape.shape)
 
         fc = tf.layers.dense(pooled_reshape, self.config.hidden_dim, activation=tf.nn.relu, name='fc1')
+        fc = tf.contrib.layers.dropout(fc, self.keep_prob)
 
-                fc = tf.contrib.layers.dropout(fc, self.keep_prob)
-        #fc = tf.nn.relu(fc)
         # 分类器
         self.logits = tf.layers.dense(fc, num_classes, name='fc2')
         self.y_pred_cls = tf.argmax(tf.nn.softmax(self.logits), 1, name="pred")  # 预测类别
@@ -53,10 +72,13 @@ class ASRCNN(object):
         self.acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     def train(argv=None):
+        '''
         batch = mfcc_batch_generator()
         X, Y = next(batch)
         trainX, trainY = X, Y
-        testX, testY = X, Y  # overfit for now'''
+        testX, testY = X, Y
+        # overfit for now
+        '''
         train_files, valid_files, test_files = load_files()
         train_features, train_labels = read_files(train_files)
         train_features = mean_normalize(train_features)
@@ -105,11 +127,12 @@ class ASRCNN(object):
                         cnn.input_y: valid_labels,
                         cnn.keep_prob: config.dropout_keep_prob})
                     print('Steps:' + str(total_batch))
-                    print(
-                            'train_loss:' + str(train_loss) + ' train accuracy:' + str(train_accuracy) + '\tvalid_loss:' + str(
-                                valid_loss) + ' valid accuracy:' + str(valid_accuracy))
-                            if total_batch % config.save_tb_per_batch == 0:
-                                train_s = session.run(merged_summary, feed_dict=feed_dict)
+                    print('train_loss:' + str(train_loss) +
+                            ' train accuracy:' + str(train_accuracy) +
+                            '\tvalid_loss:' + str(valid_loss) +
+                            ' valid accuracy:' + str(valid_accuracy))
+                if total_batch % config.save_tb_per_batch == 0:
+                    train_s = session.run(merged_summary, feed_dict=feed_dict)
                     train_writer.add_summary(train_s, total_batch)
                     valid_s = session.run(merged_summary, feed_dict={cnn.input_x: valid_features, cnn.input_y: valid_labels,
                         cnn.keep_prob: config.dropout_keep_prob})
@@ -117,12 +140,12 @@ class ASRCNN(object):
 
             saver.save(session, checkpoint_path, global_step=epoch)
 
-             test_loss, test_accuracy = session.run([cnn.loss, cnn.acc],
-                     feed_dict={cnn.input_x: test_features, cnn.input_y: test_labels,
+            test_loss, test_accuracy = session.run([cnn.loss, cnn.acc],
+                    feed_dict={cnn.input_x: test_features, cnn.input_y: test_labels,
                          cnn.keep_prob: config.dropout_keep_prob})
-                     print('test_loss:' + str(test_loss) + ' test accuracy:' + str(test_accuracy))
+            print('test_loss:' + str(test_loss) + ' test accuracy:' + str(test_accuracy))
 
-    # 测试数据准备,读取文件并提取音频特征
+# 测试数据准备,读取文件并提取音频特征
 def read_test_wave(path):
     files = os.listdir(path)
     feature = []
@@ -146,8 +169,6 @@ def test(path):
     print('loading ASRCNN model...')
     with tf.Session() as sess:
         saver = tf.train.import_meta_graph('cnn_model/model.ckpt-999.meta')
-
-
         saver.restore(sess, tf.train.latest_checkpoint('cnn_model'))  
         graph = tf.get_default_graph()
         input_x = graph.get_tensor_by_name("input_x:0")
@@ -161,3 +182,9 @@ def test(path):
             print("真实lable: %d" % label[i])
             print("识别结果为:"+str(test_output[0]))
         print("Congratulation!")  
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1] == 'train':
+        train()
+    else:
+        test()
